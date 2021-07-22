ecc5be7f27635ba0b65ec1f9c9e49261aec114bf
jasonslyvia <jasonslyvia@gmail.com>
2021-07-22 15:19:40 +0800

    refactor: move back to ant internal platform

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 </p>
 <p align="center">蚂蚁前端基础设施团队（<strong>A</strong>nt <strong>F</strong>rontend <strong>I</strong>nfrastructure）</p>
 
-主站：[https://afi.team](https://afi.team) 
+<del>
+主站：[https://afi.team](https://afi.team)
 
 镜像：[https://afi-team.github.io](https://afi-team.github.io)
 
 基于 [Gastby.js](gatsbyjs.com) 搭建，由 Aliyun OSS 托管，Aliyun CDN 提供加速服务，GitHub Pages 提供备份站点。
+</del>
+
+研发流程及源码已迁移至蚂蚁内部研发平台（没错，吃自己的狗粮🤩）
--- src/html.js
+++ src/html.js
@@ -5,6 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+      <meta http-equiv="refresh" content="0;URL='https://afi-team.alipay.com/'" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -12,9 +13,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link rel="icon" href="https://gw.alipayobjects.com/mdn/rms_f3f48a/afts/img/A*ZtzKRZPgCE8AAAAAAAAAAAAAARQnAQ" type="image/png"/>
-        <link rel="stylesheet" href="/plugins.min.css" />
-        <link rel="stylesheet" href="/base.css" />
-        <link rel="stylesheet" href="//at.alicdn.com/t/font_2373791_4tagve37lot.css" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
@@ -24,13 +22,6 @@ export default function HTML(props) {
           id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
-        <script src='https://cdn.bootcdn.net/ajax/libs/three.js/r71/three.min.js'></script>
-        <script src='https://gw.alipayobjects.com/os/lib/jquery/3.4.1/dist/jquery.min.js'></script>
-        <script src='/vendor1.js'></script>
-        <script src='/vendor2.js'></script>
-        <script src='/vendor3.js'></script>
-        <script src='/animation.js'></script>
-        <script src='/script.js'></script>
         {props.postBodyComponents}
       </body>
     </html>
