38cff9f41efb986b59c82d85ced51f94a6ba8ecf
Sen Yang <jasonslyvia@gmail.com>
2021-08-08 15:24:29 +0800

    Update html.js

--- src/html.js
+++ src/html.js
@@ -5,7 +5,6 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-      <meta http-equiv="refresh" content="0;URL=https://afi-team.alipay.com/" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -15,7 +14,14 @@ export default function HTML(props) {
         <link rel="icon" href="https://gw.alipayobjects.com/mdn/rms_f3f48a/afts/img/A*ZtzKRZPgCE8AAAAAAAAAAAAAARQnAQ" type="image/png"/>
       </head>
       <body>
-        正在重定向至 afi-team.alipay.com
+        <script type="text/javascript">
+          if (/micromessenger/.test(navigator.userAgent.toLowerCase())) {
+            document.writeLn('<p>由于众所周知的原因，微信屏蔽了支付宝的链接，请手动复制链接并在浏览器中访问 <a href="https://afi-team.alipay.com">https://afi-team.alipay.com</a> 。我们相信一个更开放的互联网早晚会到来！</p>');
+          } else {
+            document.writeLn('<p>正在重定向至 afi-team.alipay.com</p>');
+            location.href = 'https://afi-team.alipay.com';
+          }
+         </script>
       </body>
     </html>
   )
